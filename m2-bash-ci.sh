8622e80bcdf4d4c2982c639adedf29d02b56fbd6
Manuele Menozzi <manuele.menozzi@gmail.com>
2018-01-27 18:09:38 +0100

    Do not rely on SCRIPT_DIR anymore

--- m2-bash-ci.sh
+++ m2-bash-ci.sh
@@ -7,12 +7,6 @@ if [ ! -z "$1" ] && [ -f "$1" ]; then
     source "$1"
 fi;
 
-if [ -z ${SCRIPT_DIR} ]; then
-    SCRIPT_DIR="."
-fi;
-
-MAGENTO_ABSOLUTE_PATH="${SCRIPT_DIR}"
-
 function mysql_cli {
     if [ "${MYSQL_CLI_CREDENTIALS}" = true ] ; then
         mysql -h ${DB_HOST} -u ${DB_USER} --password=${DB_PASS} -e "$1"
@@ -30,13 +24,13 @@ mysql_cli "DROP DATABASE IF EXISTS \`${DB_NAME}\`;"
 mysql_cli "DROP DATABASE IF EXISTS \`${DB_TEST}\`;"
 mysql_cli "CREATE DATABASE \`${DB_NAME}\` CHARACTER SET utf8;"
 mysql_cli "CREATE DATABASE \`${DB_TEST}\` CHARACTER SET utf8;"
-php ${MAGENTO_ABSOLUTE_PATH}/bin/magento setup:install --db-host="${DB_HOST}" --db-name="${DB_NAME}" --db-user="${DB_USER}" \
+php bin/magento setup:install --db-host="${DB_HOST}" --db-name="${DB_NAME}" --db-user="${DB_USER}" \
 ${MAGENTO_INSTALL_DB_PASSWORD} --admin-firstname="Magento" --admin-lastname="User" --admin-email="user@example.com" \
 --admin-user="admin" --admin-password="admin123" --use-rewrites="1" --base-url="${BASE_URL}"
-php ${MAGENTO_ABSOLUTE_PATH}/bin/magento deploy:mode:set developer
-rm -f ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
-cp -f ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php.dist ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
-sed -i -e  "s/'db-host' => '.*'/'db-host' => '${DB_HOST}'/" ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
-sed -i -e  "s/'db-user' => '.*'/'db-user' => '${DB_USER}'/" ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
-sed -i -e  "s/'db-password' => '.*'/'db-password' => '${DB_PASS}'/" ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
-sed -i -e  "s/'db-name' => '.*'/'db-name' => '${DB_TEST}'/" ${MAGENTO_ABSOLUTE_PATH}/dev/tests/integration/etc/install-config-mysql.php
+php bin/magento deploy:mode:set developer
+rm -f dev/tests/integration/etc/install-config-mysql.php
+cp -f dev/tests/integration/etc/install-config-mysql.php.dist dev/tests/integration/etc/install-config-mysql.php
+sed -i -e  "s/'db-host' => '.*'/'db-host' => '${DB_HOST}'/" dev/tests/integration/etc/install-config-mysql.php
+sed -i -e  "s/'db-user' => '.*'/'db-user' => '${DB_USER}'/" dev/tests/integration/etc/install-config-mysql.php
+sed -i -e  "s/'db-password' => '.*'/'db-password' => '${DB_PASS}'/" dev/tests/integration/etc/install-config-mysql.php
+sed -i -e  "s/'db-name' => '.*'/'db-name' => '${DB_TEST}'/" dev/tests/integration/etc/install-config-mysql.php
